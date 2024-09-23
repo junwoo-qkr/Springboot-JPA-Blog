1ef62c8b6ebb6902357c9c24aaddf7f55ebc4795
unknown <p40212@gmail.com>
2024-09-23 21:25:27 +0900

    비밀번호 해쉬 후 회원가입하기

--- src/main/java/com/abc/blog/config/SecurityConfig.java
+++ src/main/java/com/abc/blog/config/SecurityConfig.java
@@ -18,14 +18,15 @@ public class SecurityConfig {
 @Bean // 스프링 컨테이너에서 객체를 관리할 수 있게 됨
  SecurityFilterChain configure(HttpSecurity http) throws Exception {
   http
+  	.csrf().disable() // csrf 토큰 비활성화
     .authorizeHttpRequests()
-    .requestMatchers("/auth/**") // /auth/**로 들어오는 요청은
+    .requestMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**") // /auth/**로 들어오는 요청은
     .permitAll() // 모두 허용
     .anyRequest() // 그 이외의 모든 요청은
     .authenticated() // 인증 필요
     .and()
     .formLogin()
-    .loginPage("/auth/signin"); // 인증이 필요한 요청이 오면 /auth/signin 으로 연결됨
+    .loginPage("/auth/loginForm"); // 인증이 필요한 요청이 오면 /auth/loginForm 으로 연결됨
   return http.build();
  }
 }
--- src/main/java/com/abc/blog/service/UserService.java
+++ src/main/java/com/abc/blog/service/UserService.java
@@ -1,9 +1,11 @@
 package com.abc.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.abc.blog.model.RoleType;
 import com.abc.blog.model.User;
 import com.abc.blog.repository.UserRepository;
 
@@ -12,8 +14,15 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+	
 	@Transactional 
 	public void 회원가입(User user) {
+		String rawPassword = user.getPassword(); // 비밀번호 원문
+		String encPassword = encoder.encode(rawPassword); // 해쉬화된 비밀번호
+		user.setPassword(encPassword);
+		user.setRole(RoleType.USER);
 		userRepository.save(user);
 	}
 	
