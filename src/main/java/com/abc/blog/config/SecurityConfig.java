df6020930525597b053479f9c73e41b8bb388559
unknown <p40212@gmail.com>
2024-09-24 23:20:35 +0900

    solve infinite redirection

--- src/main/java/com/abc/blog/config/SecurityConfig.java
+++ src/main/java/com/abc/blog/config/SecurityConfig.java
@@ -20,13 +20,18 @@ public class SecurityConfig {
   http
   	.csrf().disable() // csrf 토큰 비활성화
     .authorizeHttpRequests()
-    .requestMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**") // /auth/**로 들어오는 요청은
-    .permitAll() // 모두 허용
-    .anyRequest() // 그 이외의 모든 요청은
-    .authenticated() // 인증 필요
+    // .requestMatchers("/", "/WEB-INF/**", "/js/**", "/css/**", "/image/**") // 여기 적힌 url로 들어오는 요청은
+    	.requestMatchers("/", "/WEB-INF/**", "/auth/**", "/js/**", "/css/**", "/image/**") // 여기 적힌 url로 들어오는 요청은
+    	.permitAll() // 모두 허용
+    	.anyRequest() // 그 이외의 모든 요청은
+    	.authenticated() // 인증 필요
     .and()
-    .formLogin()
-    .loginPage("/auth/loginForm"); // 인증이 필요한 요청이 오면 /auth/loginForm 으로 연결됨
+    	.formLogin()
+    	.loginPage("/auth/loginForm") // 인증이 필요한 요청이 오면 /auth/loginForm 으로 연결됨
+  		.permitAll();
+  http
+  	.sessionManagement()
+  	.invalidSessionUrl("/auth/loginForm");
   return http.build();
  }
 }
